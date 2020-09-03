38bb930831c7b6f0388f2ce80574e27a16a94d13
David Lemmon <david.r.lemmon@gmail.com>
2020-09-03 11:50:23 -0400

    Added functionality to adder
    It now adds two numbers

--- adder.cs
+++ adder.cs
@@ -6,7 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+		double a, b;
 
+		a = Convert.ToDouble( args[0] );
+		b = Convert.ToDouble( args[1] );
+		Console.WriteLine("Your result: " + a + " + " + b + " = " + (a+b));
 	}
     }
 }
